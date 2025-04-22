a10ee668e649752d6aa59de96fcb2b89f8aabe86
yang_liu <yangliu2@ufl.edu>
2025-04-22 16:45:21 -0400

    Update ReverseList.hpp

--- ReverseList.hpp
+++ ReverseList.hpp
@@ -30,8 +30,21 @@ public:
         else
         {
             std::vector<int> allData;
+            Node* currentNode = head;
 
-            
+            while(currentNode != nullptr)
+            {
+                allData.push_back(currentNode->data);
+                currentNode = currentNode->next;
+            }
+
+            for (size_t i = 0; i < allData.size(); i++)
+            {
+                result.addHead(allData[i]);
+            }
+        }
+
+        return result;
     }
 
     Node* head = nullptr;
