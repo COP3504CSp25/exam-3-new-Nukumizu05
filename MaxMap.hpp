acf6d325a27fdeaf6601a90ed1aa1c8a74f2e8f1
yang_liu <yangliu2@ufl.edu>
2025-04-22 16:51:36 -0400

    Update MaxMap.hpp

--- MaxMap.hpp
+++ MaxMap.hpp
@@ -5,8 +5,24 @@
 #include <string>
 #include <vector>
 
-float consultMax(const std::string& search_term, const std::map<std::string, std::vector<float>>& data) {
-    //implement your function here
+float consultMax(const std::string& search_term, const std::map<std::string, std::vector<float>>& data) 
+{
+    float max_val = -1;
+    
+    std::map<std::string, std::vector<float>>::iterator it = data.begin();
+
+    for (it; it != data.end(); ++it)
+    {
+        if (data->first == search_term)
+        {
+            for (size_t i = 0; i < data->second.size(); i++)
+            {
+                max_val = data->second.at(i) > max_val ? data->second.at(i) : max_val;
+            }
+        }
+    }
+
+    return max_val;
 }
 
 std::map<std::string, float> returnMaxMap(const std::map<std::string, std::vector<float>>& in_map) {
