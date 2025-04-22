ccb0f45b5b9a4d19520d35983691bf14501e3ade
yang_liu <yangliu2@ufl.edu>
2025-04-22 16:35:28 -0400

    Update GetStats.hpp

--- GetStats.hpp
+++ GetStats.hpp
@@ -1,4 +1,9 @@
 #pragma once
+
+#include <iostream>
+#include <climit>
+#include <vector>
+
 struct Node {
     int data;
     Node* next;
@@ -13,8 +18,32 @@ public:
         head = newNode;
     }
 
-    int* getStats() const{
-        //write your function here!
+    int* getStats() const
+    {
+        auto array = new int[3];
+
+        std::vector<int> allData;
+        int sum = 0;
+        int count = 0;
+        int min_val = INT_MAX;
+        int max_val = INT_MIN;
+
+        Node* currentNode = head;
+        while(currentNode != nullptr)
+        {
+            int num = currentNode->data;
+            sum += num;
+            min_val = num < min_val ? num : min_val;
+            max_val = num > max_val ? num : max_val;
+            count++;
+            currentNode = currentNode->next;
+        }
+
+        array[0] = min_val;
+        array[1] = max_val;
+        array[2] = sum / count;
+
+        return array;
     }
     
 
